ccea0fc234737dc6488af200938702273193dfaf
chrisdevor <chrisdevor@macroplant.com>
2020-11-06 07:46:07 -0500

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -17,6 +17,13 @@ cd ~ && {
 	echo '[[ -s ${HOME}/repos/dochub/.bash ]] && source ${HOME}/repos/dochub/.bash' >>! ~/.zshrc
 	echo '[[ -s ${HOME}/repos/iexplorer/.bash ]] && source ${HOME}/repos/iexplorer/.bash' >>! ~/.zshrc
 	echo '[[ -s ${HOME}/repos/macroplant-rails2/.bash ]] && source ${HOME}/repos/macroplant-rails2/.bash' >>! ~/.zshrc
-cd -; }
 
-# grep -qxF 'include "/configs/projectname.conf"' ~/.zshrc ||
+	# Load pyenv automatically by appending
+	echo 'eval "$(pyenv init -)"' >>! ~/.zshrc
+
+
+	# The next line updates PATH for the Google Cloud SDK.
+	echo "if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then . '~/google-cloud-sdk/path.zsh.inc'; fi" >>! ~/.zshrc
+	# The next line enables shell command completion for gcloud.
+	echo "if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi" >>! ~/.zshrc
+cd -; }
